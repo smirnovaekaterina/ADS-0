9034021cbb50f5296bf8705ade91e03bc4628856
smirnovaekaterina <75855500+smirnovaekaterina@users.noreply.github.com>
2021-03-16 11:57:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,24 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+int cbinsearch(int *arr, int size, int value) {
+int i = 0, j = size - 1, c = 0;
+while (i < j) {
+int m = (i + j) / 2;
+if (arr[m] < value)
+i = m + 1;
+else
+j = m;
+}
+
+if (arr[i] == value) {
+while (arr[j] == value) {
+c++;
+i++;
+}
+}
+
+if (c)
+return c;
+else
+return 0; // если ничего не найдено
 }
